ee4f33fb58b15352313b1afbd735dcf9a1de6fab
ansiboy <ansiboy@163.com>
2015-06-17 20:00:44 +0800

    重新压缩 crossroads.min.js 文件

--- Samples/Index.js
+++ Samples/Index.js
@@ -2,9 +2,10 @@
     baseUrl: 'Scripts',
     paths: {
         jquery: 'jquery-2.1.1.min',
+        crossroads: 'crossroads.min'
     }
 });
-requirejs(['jquery-2.1.1.min', 'crossroads', 'chitu'], function () {
+requirejs(['chitu'], function () {
     var app = new chitu.Application(function (options) {
         options.actionPath = '../Modules/{controller}/{action}';
         options.viewPath = '../Modules/{controller}/{action}';
