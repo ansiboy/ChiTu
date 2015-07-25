282af14cd00a34e466791ea3af4fbfdbfaf5d58d
ansiboy <ansiboy@163.com>
2015-07-25 21:20:32 +0800

    使用 typescript 重写

--- ChiTu/Extends.js
+++ ChiTu/Extends.js
@@ -1,200 +1,226 @@
-﻿(function (ns) {
-    var e = ns.Error;
-
-    /// 改编自 jQuery.Callbacks
-    ns.Callbacks = function (options) {
-
+var chitu;
+(function (chitu) {
+    var rnotwhite = (/\S+/g);
+    // String to Object options format cache
+    var optionsCache = {};
+    // Convert String-formatted options into Object-formatted ones and store in cache
+    function createOptions(options) {
+        var object = optionsCache[options] = {};
+        jQuery.each(options.match(rnotwhite) || [], function (_, flag) {
+            object[flag] = true;
+        });
+        return object;
+    }
+    var Callback = (function () {
+        function Callback(source) {
+            this.source = source;
+        }
+        Callback.prototype.add = function (func) {
+            this.source.add(func);
+        };
+        Callback.prototype.remove = function (func) {
+            this.source.remove(func);
+        };
+        Callback.prototype.has = function (func) {
+            return this.source.has(func);
+        };
+        Callback.prototype.fireWith = function (context, args) {
+            return this.source.fireWith(context, args);
+        };
+        Callback.prototype.fire = function (arg1, arg2, arg3, arg4) {
+            return this.source.fire(arg1, arg2, arg3);
+        };
+        return Callback;
+    })();
+    chitu.Callback = Callback;
+    function Callbacks(options) {
+        if (options === void 0) { options = null; }
         // Convert options from String-formatted to Object-formatted if needed
         // (we check in cache first)
         options = typeof options === "string" ?
             (optionsCache[options] || createOptions(options)) :
             jQuery.extend({}, options);
-
-        var // Last fire value (for non-forgettable lists)
-            memory,
-            // Flag to know if list was already fired
-            fired,
-            // Flag to know if list is currently firing
-            firing,
-            // First callback to fire (used internally by add and fireWith)
-            firingStart,
-            // End of the loop when firing
-            firingLength,
-            // Index of currently firing callback (modified by remove if needed)
-            firingIndex,
-            // Actual callback list
-            list = [],
-            // Stack of fire calls for repeatable lists
-            stack = !options.once && [],
-            // Fire callbacks
-            fire = function (data) {
-                memory = options.memory && data;
-                fired = true;
-                firingIndex = firingStart || 0;
-                firingStart = 0;
-                firingLength = list.length;
-                firing = true;
-                for (; list && firingIndex < firingLength; firingIndex++) {
-                    var result = list[firingIndex].apply(data[0], data[1]);
-                    //==============================================
-                    // MY CODE
-                    if (result != null) {
-                        data[0].results.push(result);
-                    }
-                    //==============================================
-                    if (result === false && options.stopOnFalse) {
-                        memory = false; // To prevent further calls using add
-                        break;
+        var memory, 
+        // Flag to know if list was already fired
+        fired, 
+        // Flag to know if list is currently firing
+        firing, 
+        // First callback to fire (used internally by add and fireWith)
+        firingStart, 
+        // End of the loop when firing
+        firingLength, 
+        // Index of currently firing callback (modified by remove if needed)
+        firingIndex, 
+        // Actual callback list
+        list = [], 
+        // Stack of fire calls for repeatable lists
+        stack = !options.once && [], 
+        // Fire callbacks
+        fire = function (data) {
+            memory = options.memory && data;
+            fired = true;
+            firingIndex = firingStart || 0;
+            firingStart = 0;
+            firingLength = list.length;
+            firing = true;
+            for (; list && firingIndex < firingLength; firingIndex++) {
+                var result = list[firingIndex].apply(data[0], data[1]);
+                //==============================================
+                // MY CODE
+                if (result != null) {
+                    data[0].results.push(result);
+                }
+                //==============================================
+                if (result === false && options.stopOnFalse) {
+                    memory = false; // To prevent further calls using add
+                    break;
+                }
+            }
+            firing = false;
+            if (list) {
+                if (stack) {
+                    if (stack.length) {
+                        fire(stack.shift());
                     }
                 }
-                firing = false;
+                else if (memory) {
+                    list = [];
+                }
+                else {
+                    self.disable();
+                }
+            }
+        }, 
+        // Actual Callbacks object
+        self = {
+            results: [],
+            // Add a callback or a collection of callbacks to the list
+            add: function () {
                 if (list) {
-                    if (stack) {
-                        if (stack.length) {
-                            fire(stack.shift());
-                        }
-                    } else if (memory) {
-                        list = [];
-                    } else {
-                        self.disable();
-                    }
-                }
-            },
-            // Actual Callbacks object
-            self = {
-                results: [],
-                // Add a callback or a collection of callbacks to the list
-                add: function () {
-                    if (list) {
-                        // First, we save the current length
-                        var start = list.length;
-                        (function add(args) {
-                            jQuery.each(args, function (_, arg) {
-                                var type = jQuery.type(arg);
-                                if (type === "function") {
-                                    if (!options.unique || !self.has(arg)) {
-                                        list.push(arg);
-                                    }
-                                } else if (arg && arg.length && type !== "string") {
-                                    // Inspect recursively
-                                    add(arg);
-                                }
-                            });
-                        })(arguments);
-                        // Do we need to add the callbacks to the
-                        // current firing batch?
-                        if (firing) {
-                            firingLength = list.length;
-                            // With memory, if we're not firing then
-                            // we should call right away
-                        } else if (memory) {
-                            firingStart = start;
-                            fire(memory);
-                        }
-                    }
-                    return this;
-                },
-                // Remove a callback from the list
-                remove: function () {
-                    if (list) {
-                        jQuery.each(arguments, function (_, arg) {
-                            var index;
-                            while ((index = jQuery.inArray(arg, list, index)) > -1) {
-                                list.splice(index, 1);
-                                // Handle firing indexes
-                                if (firing) {
-                                    if (index <= firingLength) {
-                                        firingLength--;
-                                    }
-                                    if (index <= firingIndex) {
-                                        firingIndex--;
-                                    }
+                    // First, we save the current length
+                    var start = list.length;
+                    (function add(args) {
+                        jQuery.each(args, function (_, arg) {
+                            var type = jQuery.type(arg);
+                            if (type === "function") {
+                                if (!options.unique || !self.has(arg)) {
+                                    list.push(arg);
                                 }
                             }
+                            else if (arg && arg.length && type !== "string") {
+                                // Inspect recursively
+                                add(arg);
+                            }
                         });
+                    })(arguments);
+                    // Do we need to add the callbacks to the
+                    // current firing batch?
+                    if (firing) {
+                        firingLength = list.length;
                     }
-                    return this;
-                },
-                // Check if a given callback is in the list.
-                // If no argument is given, return whether or not list has callbacks attached.
-                has: function (fn) {
-                    return fn ? jQuery.inArray(fn, list) > -1 : !!(list && list.length);
-                },
-                // Remove all callbacks from the list
-                empty: function () {
-                    list = [];
-                    firingLength = 0;
-                    return this;
-                },
-                // Have the list do nothing anymore
-                disable: function () {
-                    list = stack = memory = undefined;
-                    return this;
-                },
-                // Is it disabled?
-                disabled: function () {
-                    return !list;
-                },
-                // Lock the list in its current state
-                lock: function () {
-                    stack = undefined;
-                    if (!memory) {
-                        self.disable();
+                    else if (memory) {
+                        firingStart = start;
+                        fire(memory);
                     }
-                    return this;
-                },
-                // Is it locked?
-                locked: function () {
-                    return !stack;
-                },
-                // Call all callbacks with the given context and arguments
-                fireWith: function (context, args) {
-                    context.results = [];
-                    if (list && (!fired || stack)) {
-                        args = args || [];
-                        args = [context, args.slice ? args.slice() : args];
-                        if (firing) {
-                            stack.push(args);
-                        } else {
-                            fire(args);
-                        }
-                    }
-                    return context.results;
-                },
-                // Call all the callbacks with the given arguments
-                fire: function () {
-                    return self.fireWith(this, arguments);
-                },
-                // To know if the callbacks have already been called at least once
-                fired: function () {
-                    return !!fired;
-                },
-                count: function () {
-                    return list.length;
                 }
-            };
-
-        return self;
-    };
-
-    ns.fireCallback = function (callback, args) {
-        /// <param name="callback" type="chitu.Callback"/>
-        /// <param name="args" type="Array"/>
-
-        var results = callback.fire.apply(callback, args) || [];
+                return this;
+            },
+            // Remove a callback from the list
+            remove: function () {
+                if (list) {
+                    jQuery.each(arguments, function (_, arg) {
+                        var index;
+                        while ((index = jQuery.inArray(arg, list, index)) > -1) {
+                            list.splice(index, 1);
+                            // Handle firing indexes
+                            if (firing) {
+                                if (index <= firingLength) {
+                                    firingLength--;
+                                }
+                                if (index <= firingIndex) {
+                                    firingIndex--;
+                                }
+                            }
+                        }
+                    });
+                }
+                return this;
+            },
+            // Check if a given callback is in the list.
+            // If no argument is given, return whether or not list has callbacks attached.
+            has: function (fn) {
+                return fn ? jQuery.inArray(fn, list) > -1 : !!(list && list.length);
+            },
+            // Remove all callbacks from the list
+            empty: function () {
+                list = [];
+                firingLength = 0;
+                return this;
+            },
+            // Have the list do nothing anymore
+            disable: function () {
+                list = stack = memory = undefined;
+                return this;
+            },
+            // Is it disabled?
+            disabled: function () {
+                return !list;
+            },
+            // Lock the list in its current state
+            lock: function () {
+                stack = undefined;
+                if (!memory) {
+                    self.disable();
+                }
+                return this;
+            },
+            // Is it locked?
+            locked: function () {
+                return !stack;
+            },
+            // Call all callbacks with the given context and arguments
+            fireWith: function (context, args) {
+                context.results = [];
+                if (list && (!fired || stack)) {
+                    args = args || [];
+                    args = [context, args.slice ? args.slice() : args];
+                    if (firing) {
+                        stack.push(args);
+                    }
+                    else {
+                        fire(args);
+                    }
+                }
+                return context.results;
+            },
+            // Call all the callbacks with the given arguments
+            fire: function () {
+                return self.fireWith(this, arguments);
+            },
+            // To know if the callbacks have already been called at least once
+            fired: function () {
+                return !!fired;
+            },
+            count: function () {
+                return list.length;
+            }
+        };
+        return new chitu.Callback(self);
+    }
+    chitu.Callbacks = Callbacks;
+    function fireCallback(callback, args) {
+        var results = callback.fire.apply(callback, args);
         var deferreds = [];
         for (var i = 0; i < results.length; i++) {
-            if (chitu.utility.isDeferred(results[i]))
+            if (chitu.Utility.isDeferred(results[i]))
                 deferreds.push(results[i]);
         }
-
         if (deferreds.length == 0)
             return $.Deferred().resolve();
-
         return $.when.apply($, deferreds);
     }
-
-    //crossroads.create
+    chitu.fireCallback = fireCallback;
+    var crossroads = window['crossroads'];
     $.extend(crossroads, {
         _create: crossroads.create,
         create: function () {
@@ -203,32 +229,22 @@
             obj.getRouteData = function (request, defaultArgs) {
                 request = request || '';
                 defaultArgs = defaultArgs || [];
-
                 // should only care about different requests if ignoreState isn't true
                 if (!this.ignoreState &&
                     (request === this._prevMatchedRequest ||
-                     request === this._prevBypassedRequest)) {
+                        request === this._prevBypassedRequest)) {
                     return;
                 }
-
-                var routes = this._getMatchedRoutes(request),
-                    i = 0,
-                    n = routes.length,
-                    cur;
-
+                var routes = this._getMatchedRoutes(request), i = 0, n = routes.length, cur;
                 if (n == 0)
                     return null;
-
                 if (n > 1) {
-                    throw e.ambiguityRouteMatched(request, 'route1', 'route2');
+                    throw chitu.Errors.ambiguityRouteMatched(request, 'route1', 'route2');
                 }
                 return routes[0];
-            }
+            };
             return obj;
         }
     });
-
-
-
-
-})(chitu);
+})(chitu || (chitu = {}));
+//# sourceMappingURL=Extends.js.map
--- ChiTu/Utility.js
+++ ChiTu/Utility.js
@@ -1,37 +1,25 @@
-﻿window.chitu = window.chitu || {};
-(function (ns) {
-    var e = ns.Error;
-    ns.utility = {
-        isType: function (type, obj) {
-            /// <param name="type" type="Function"/>
-            if (!type) throw e.argumentNull('type');
-            if (!$.isFunction(type)) throw e.paramTypeError('type', 'Function');
-            if (!obj) throw e.argumentNull('obj');
-
-            for (var key in type.prototype) {
+/// <reference path="scripts/typings/jquery/jquery.d.ts" />
+var chitu;
+(function (chitu) {
+    var e = chitu.Errors;
+    var Utility = (function () {
+        function Utility() {
+        }
+        Utility.isType = function (targetType, obj) {
+            for (var key in targetType.prototype) {
                 if (obj[key] === undefined)
                     return false;
             }
             return true;
-        },
-        isDeferred: function (obj) {
-            /// <param name="obj" type="Object"/>
+        };
+        Utility.isDeferred = function (obj) {
             if (obj == null)
                 return false;
-
             if (obj.pipe != null && obj.always != null && obj.done != null)
                 return true;
-
             return false;
-        },
-        format: function (source, params) {
-            if (arguments.length === 1) {
-                return function () {
-                    var args = $.makeArray(arguments);
-                    args.unshift(source);
-                    return $.validator.format.apply(this, args);
-                };
-            }
+        };
+        Utility.format = function (source, params) {
             if (arguments.length > 2 && params.constructor !== Array) {
                 params = $.makeArray(arguments).slice(1);
             }
@@ -44,36 +32,38 @@
                 });
             });
             return source;
-        },
-        fileName: function (url, withExt) {
+        };
+        Utility.fileName = function (url, withExt) {
             /// <summary>获取 URL 链接中的文件名</summary>
             /// <param name="url" type="String">URL 链接</param>
             /// <param name="withExt" type="Boolean" canBeNull="true">
             /// 表示返回的文件名是否包含扩展名，true表示包含，false表示不包含。默认值为true。
             /// </param>
             /// <returns>返回 URL 链接中的文件名</returns>
-            if (!url) throw e.argumentNull('url');
+            if (!url)
+                throw e.argumentNull('url');
             withExt = withExt || true;
-
             url = url.replace('http://', '/');
             var filename = url.replace(/^.*[\\\/]/, '');
             if (withExt === true) {
                 var arr = filename.split('.');
                 filename = arr[0];
             }
-
             return filename;
-        },
-        log: function (msg, args) {
-            if (!window.console) return;
-
+        };
+        Utility.log = function (msg, args) {
+            if (args === void 0) { args = []; }
+            if (!window.console)
+                return;
             if (args == null) {
                 console.log(msg);
                 return;
             }
             var txt = this.format.apply(this, arguments);
             console.log(txt);
-        }
-
-    };
-})(chitu);
+        };
+        return Utility;
+    })();
+    chitu.Utility = Utility;
+})(chitu || (chitu = {}));
+//# sourceMappingURL=Utility.js.map
