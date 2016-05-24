34af70d091d79a1d847921b9d065f5f29e4c7c0c
yunde <yunde@yunde-mac-mini.local>
2016-05-24 09:53:53 +0800

    清理无用的注释代码

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,8 +62,8 @@ module.exports = function(grunt) {
     config.copy = {
         main: {
             files: [///Users/MaiShu/git/ChiTuStore/src/Scripts 
-                { src: [js_output_file], dest: '/Users/MaiShu/git/ChiTuStore/src/Scripts/chitu.js' },
-                { src: [ts_output_file], dest: '/Users/MaiShu/git/ChiTuStore/src/Scripts/typings/chitu.d.ts' },
+                { src: [js_output_file], dest: '../ChiTuStore/src/Scripts/chitu.js' },
+                { src: [ts_output_file], dest: '../ChiTuStore/src/Scripts/typings/chitu.d.ts' },
             ]
         }
     };
