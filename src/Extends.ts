55d6788a5ffe7249d7a5789a20cfd5b5c8e1dc29
maishu <ansiboy@163.com>
2017-11-19 21:37:03 +0800

    ValueStore 添加 sender 参数

--- src/Extends.ts
+++ src/Extends.ts
@@ -26,22 +26,23 @@ namespace chitu {
     //==========================================================
 
     /** 实现数据的存储，以及数据修改的通知 */
+    export type ValueChangedCallback<T> = (args: T, sender: any) => void;
     export class ValueStore<T> {
-        private funcs = new Array<(args: T) => void>();
+        private items = new Array<{ func: ValueChangedCallback<T>, sender: any }>();
         private _value: T;
 
         constructor(value?: T) {
             this._value = value;
         }
-        add(func: (value: T) => any): (args: T) => any {
-            this.funcs.push(func);
+        add(func: ValueChangedCallback<T>, sender?: any): ValueChangedCallback<T> {
+            this.items.push({ func, sender });
             return func;
         }
-        remove(func: (value: T) => any) {
-            this.funcs = this.funcs.filter(o => o != func);
+        remove(func: ValueChangedCallback<T>) {
+            this.items = this.items.filter(o => o.func != func);
         }
         fire(value: T) {
-            this.funcs.forEach(o => o(value));
+            this.items.forEach(o => o.func(value, o.sender));
         }
         get value(): T {
             return this._value;
