c0039d274a746747fcff9a7d2cef015067e8f2d4
maishu <ansiboy@163.com>
2019-10-12 11:06:42 +0800

    remove files

--- out/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { Application, PageNodeParser, Action, PageNode, parseUrl, createPageUrl } from './Application';
-export { PageMaster, } from './PageMaster';
-export { Page, PageConstructor, PageData } from './Page';
-export { Callback, Callback1, Callback2, Callbacks, ValueStore, AjaxOptions, ServiceConstructor, IService, Service } from 'maishu-chitu-service';
